ea3d59c23fb5a138a65692723f9c31ffa0e5663b
Roman Isaikin <romix.lab@gmail.com>
2025-12-22 14:30:03 +0100

    refactor: use usb4604 from crates.io

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+## [0.1.0]
+
+### 🚀 Features
+
+- Read RAM and NVM
+- Write_masked()
+- Matrix inputs
+- Matrix outputs
+- read_ram_masked()
+- matrix_wire()
+- Blinky and memory dump examples
+
+### 📚 Documentation
+
+- Update README
--- examples/blinky.rs
+++ examples/blinky.rs
@@ -10,7 +10,7 @@ fn main() -> Result<()> {
     let usb4604 = Usb4604::open_auto()?;
 
     // Enable I2C pull-up resistors after USB4604 is booted (i.e., do not tie them to 3V3, otherwise it won't boot)
-    let _pull_up_en = usb4604.output(Pio::Pio9, Level::High)?;
+    let _pull_up_en = usb4604.output(Pio::Pio9, Some(Level::High))?;
 
     let i2c = usb4604.i2c_bridge()?;
     let mut slg = Slg46824x::new(i2c, 1).expect("");
